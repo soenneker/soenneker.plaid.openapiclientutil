48592442b366b74c75e20ab902da827fa6a9f7c6
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:35 -0500

    xml commenting

--- src/Soenneker.Plaid.OpenApiClientUtil/Abstract/IPlaidOpenApiClientUtil.cs
+++ src/Soenneker.Plaid.OpenApiClientUtil/Abstract/IPlaidOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Plaid.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IPlaidOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<PlaidOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
